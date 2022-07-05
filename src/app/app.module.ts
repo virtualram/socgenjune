298d132b3bc6d851e6d149c447e0848f7b99dbdc
virtualram <ramnarayananrr@yahoo.com>
2022-07-05 16:06:05 +0530

    menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,28 @@ import { NgModel } from '@angular/forms';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { Form1Component } from './form1/form1.component';
 import { HttpClient, HttpClientModule   } from '@angular/common/http';
+import { MenuComponent } from './menu/menu.component';
+import { Routes, RouterModule ,ActivatedRoute } from '@angular/router';
+let  routes: Routes = [
+ {path: 'Form', component:Form1Component},
+ {path: 'Sub', component:SubComponent},
+{path:'App',component:AppComponent}
+
+];
 @NgModule({
   declarations: [
     AppComponent,
     SubComponent,
-    Form1Component
+    Form1Component,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,ReactiveFormsModule,
-    HttpClientModule  
+    HttpClientModule  ,FormsModule,ReactiveFormsModule,RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MenuComponent]
 })
 export class AppModule { }
